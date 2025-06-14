143d951aa6ac1df94f2c4885464480147da84041
hunter <hunterjsb@gmail.com>
2025-06-14 13:23:40 -0400

    generic scraper implementation

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from scout!")
+import streamlit as st
+import numpy as np
 
+col1, col2, col3 = st.columns([1, 1, 3])
+data = np.random.randn(10, 1)
 
-if __name__ == "__main__":
-    main()
+col1.subheader("A wide column with a chart")
+col2.write(data)
+
+col2.subheader("A narrow column with the data")
+col2.write(data)
+
+col3.subheader("A wide column with a chart")
+col3.line_chart(data)
